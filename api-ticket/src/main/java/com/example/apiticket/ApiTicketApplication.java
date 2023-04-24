06da896526043e4072b7c54ce24bc24250bf9d73
Ailton Ferreira Luz <djailtim@msn.com>
2023-04-23 22:36:36 -0300

    API Ticket: create project ticket

---
+++ api-ticket/src/main/java/com/example/apiticket/ApiTicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTicketApplication.class, args);
+    }
+
+}
---
+++ api-ticket/src/test/java/com/example/apiticket/ApiTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
